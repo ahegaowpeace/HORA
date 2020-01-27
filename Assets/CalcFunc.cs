f7a5c2a03fa20ce08a0dd08b51955301c9350a7c
ahegaowpeace <arduino0202@gmail.com>
2020-01-27 21:48:25 +0900

    役選択/ボタンイベント作成。残りは符計算部分UIや計算/表示処理など。

---
+++ Assets/CalcFunc.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalcFunc : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// 点数計算関数
+	public void OnClick () {
+		Debug.Log("ボタンが押されました");
+	}
+}
---
+++ Assets/DeleteFunc.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeleteFunc : MonoBehaviour {
+
+	private Button DeleteButton;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// 結果消去
+	public void OnClick () {
+		Debug.Log("結果が消去されました");
+	}
+}
